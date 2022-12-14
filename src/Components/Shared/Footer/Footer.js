9e7ec83c1f4dfa37a1eb6919e04784399da49ec4
juhayerayaz <aiazj97@gmail.com>
2022-12-15 01:13:07 +0600

    login and join added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import insider from '../../Imgs/insider-logo.png'
+import marvelU from '../../Imgs/marvel-u.png'
+import { BsPinterest, BsSnapchat } from 'react-icons/bs'
+import { AiFillFacebook, AiFillYoutube } from 'react-icons/ai'
+import { FaTwitter, FaTumblr } from 'react-icons/fa'
+import { ImInstagram } from 'react-icons/im'
+
 
 const Footer = () => {
     return (
-        <footer>
-            <span class="icon--svg main-footer__logo--svg" aria-hidden="true"><svg viewBox="0 0 36 52" xmlns="http://www.w3.org/2000/svg"><rect fill="#EC1D24" width="100%" height="100%"></rect><path fill="#FEFEFE" d="M31.5 48V4H21.291l-3.64 22.735L14.102 4H4v44h8V26.792L15.577 48h4.229l3.568-21.208V48z"></path></svg></span>
+        <footer className='bg-[#151515] pt-20 pb-8'>
+            <div className='mx-40'>
+                <div className='flex flex-row items-center justify-between px-5 mb-20'>
+                    <span>
+                        <svg viewBox="0 0 36 52" xmlns="http://www.w3.org/2000/svg" className="w-[80px]">
+                            <rect fill="#EC1D24" width="100%" height="100%"></rect>
+                            <path fill="#FEFEFE" d="M31.5 48V4H21.291l-3.64 22.735L14.102 4H4v44h8V26.792L15.577 48h4.229l3.568-21.208V48z"></path>
+                        </svg>
+                    </span>
+                    <div>
+                        <a href="https://www.marvel.com/corporate/about" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className='font-bold'>About Marvel</h1></a>
+                        <a href="https://help.marvel.com/" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className='font-bold'>Helps/Faqs</h1></a>
+                        <a href="https://jobs.disneycareers.com/search-jobs?orgIds=391-1635-24091&ascf=[{%22key%22:%22custom_fields%252EIndustryCustomField%22,%22value%22:%22Marvel%20Entertainment%22}]" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className='font-bold'>Careers</h1></a>
+                        <a href="https://www.marvel.com/corporate/interns" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className='font-bold'>Internships</h1></a>
+                    </div>
+                    <div className='mr-10'>
+                        <a href="https://www.marvel.com/corporate/advertising" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className=''>Advertising</h1></a>
+                        <a href="https://www.disneyplus.com/brand/marvel?cid=DTCI-Synergy-Marvel-Site-Acquisition-Library-US-Marvel-NA-EN-NavFooter-Marvel_DisneyPlus_NavFooter_Evergreen-NA" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className=''>Disney+</h1></a>
+                        <a href="https://www.marvelhq.com/" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className=''>Marvelhq.com</h1></a>
+                        <a href="https://www.marvel.com/redeem" className='text-white hover:text-zinc-400' target={'_blank'} rel="noreferrer"><h1 className=''>Redeem Digital Comics</h1></a>
+                    </div>
+                    <div className='mx-10'>
+                        <div className='flex items-center mb-5'>
+                            <img src={insider} className="w-[60px] mr-10" alt="" />
+                            <div>
+                                <h1 className='text-white font-bold'>Marvel Insider</h1>
+                                <p className='text-sm text-[#767676]'>Get Rewarded for being a marvel fan</p>
+                            </div>
+                        </div>
+                        <div className='flex items-center'>
+                            <img src={marvelU} className="w-[60px] mr-10" alt="" />
+                            <div>
+                                <h1 className='text-white font-bold'>Marvel Unlimited</h1>
+                                <p className='text-sm text-[#767676]'>Access over 30,000+ Digital Comics</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <h1 className='font-bold text-white mb-4'>Follow Marvel</h1>
+                        <div className='text-[#767676] grid grid-cols-4 gap-5'>
+                            <AiFillFacebook className='w-[28px] h-[28px] hover:text-white cursor-pointer' />
+                            <FaTwitter className='w-[24px] h-[24px] hover:text-white cursor-pointer' />
+                            <ImInstagram className='w-[22px] h-[22px] hover:text-white cursor-pointer' />
+                            <FaTumblr className='w-[22px] h-[22px] hover:text-white cursor-pointer' />
+                            <AiFillYoutube className='w-[28px] h-[28px] hover:text-white cursor-pointer' />
+                            <BsSnapchat className='w-[22px] h-[22px] hover:text-white cursor-pointer' />
+                            <BsPinterest className='w-[22px] h-[22px] hover:text-white cursor-pointer' />
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center justify-around px-10'>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Terms of use</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Privacy Policy</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Your California Rights</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Childrens Online Privacy Policy</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>License Agreement</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Interest Based Ads</p>
+                    <p className='text-sm text-[#767676] hover:text-white cursor-pointer'>Marvel Insider Terms</p>
+                    <p className='text-sm text-[#767676]'>&#169; 2022 MARVEL</p>
+                </div>
+            </div>
         </footer>
     );
 };
--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,17 +1,92 @@
 import React from 'react';
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom';
 import logo from '../../Imgs/marvel-logo.png'
+import threelogo from '../../Imgs/three-logo.png'
+import { useForm } from 'react-hook-form';
+
 
 const Navbar = () => {
+    const joinModal = "join-modal"
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
+
     return (
         <div className='bg-[#202020]'>
             <div className='flex items-center justify-center border border-[#393939] border-x-0 border-t-0 h-[52px]'>
                 <div className='flex items-center border border-[#393939] p-3 border-y-0 text-white'>
                     <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill='white' viewBox="0 0 65.6 66.7"><path fill-rule="evenodd" d="M59.5 15.1L53 51.4c0 .4-.3.6-.6.6h-6.5c-.3 0-.6-.2-.8-.6l-8.8-24.7H36l-4.3 24.7c-.1.3-.3.6-.6.6H25c-.3 0-.6-.2-.5-.6l6.6-37.3c.1-.3.3-.6.6-.6h6.4c.5 0 .7.2.8.6l8.8 24.7h.3l5.5-30.4C47.9 3.7 40.6.9 32.7.9 14.7.9.2 15.5.2 33.4c0 8.8 3.5 16.7 9.1 22.5l7.3-41.7c.1-.3.3-.6.6-.6H24c.3 0 .5.2.5.6l-8.3 47.2c4.8 2.8 10.4 4.5 16.4 4.5 17.9 0 32.5-14.5 32.5-32.5 0-6.8-2.1-13.1-5.6-18.3z"></path></svg>
-                    <Link className='mx-2 text-xs font-semibold cursor-pointer' style={{ letterSpacing: '1px' }}>SIGN IN</Link>
+                    <label htmlFor="login-modal" className="mx-2 text-xs font-semibold cursor-pointer uppercase" style={{ letterSpacing: '1px' }}>Sign in</label>
                     |
-                    <Link className='ml-2 text-xs font-semibold' style={{ letterSpacing: '1px' }}>JOIN</Link>
+                    <input type="checkbox" id="login-modal" className="modal-toggle" />
+                    <div className="modal modal-bottom sm:modal-middle bg-black bg-opacity-90">
+                        <div className="card w-[409px] bg-white px-10 py-9 rounded-none">
+                            <label htmlFor="login-modal" className="text-black text-xl p-3 font-bold cursor-pointer hover:text-stone-500 bg-transparent absolute right-2 top-2">✕</label>
+                            <div>
+                                <img className='h-[56px] w-[200px] mx-auto mb-5 mt-7' src={threelogo} alt="" />
+                                <h1 className='text-red-600 uppercase text-lg text-center'>Sign In</h1>
+                                <form onSubmit={handleSubmit(onSubmit)} className="space-y-8 py-6">
+                                    <div>
+                                        <input
+                                            type="email"
+                                            placeholder="Username or Email Address"
+                                            className="w-full text-black py-3 px-6 ring-1 ring-gray-300 rounded placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+                                            {...register("email", {
+                                                required: {
+                                                    value: true,
+                                                    message: 'Email is Required'
+                                                },
+                                                pattern: {
+                                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                    message: 'Provide a valid Email'
+                                                }
+                                            })}
+                                        />
+                                        <label className="label">
+                                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                        </label>
+                                    </div>
+
+                                    <div className="flex flex-col items-end">
+                                        <input
+                                            type="password"
+                                            placeholder="Enter Password"
+                                            className="w-full text-black py-3 px-6 ring-1 ring-gray-300 rounded placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+                                            {...register("password", {
+                                                required: {
+                                                    value: true,
+                                                    message: 'Password is Required'
+                                                },
+                                                minLength: {
+                                                    value: 6,
+                                                    message: 'Must be 6 characters or longer'
+                                                }
+                                            })}
+                                        />
+                                        <label className="label">
+                                            {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                            {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                        </label>
+                                    </div>
+                                    <input className='w-full px-6 py-3 my-4 rounded-none bg-red-600 btn hover:bg-red-800' type="submit" value="Login" />
+                                </form>
+                                <label className='text-black border-2 font-semibold border-black px-[104px] p-3 cursor-pointer' htmlFor={joinModal}>Create an Account</label>
+                            </div>
+                        </div>
+                    </div>
+                    <label htmlFor={joinModal} className="mx-2 text-xs font-semibold cursor-pointer uppercase" style={{ letterSpacing: '1px' }}>JOIN</label>
+                    <input type="checkbox" id={joinModal} className="modal-toggle" />
+                    <div className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg text-black">Congratulations Join Internet user!</h3>
+                            <p className="py-4 text-black">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                            <div className="modal-action">
+                                <label htmlFor={joinModal} className="btn">Yay!</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='mx-[22rem]'>
                     <img className='h-[52px]' src={logo} alt="" />
