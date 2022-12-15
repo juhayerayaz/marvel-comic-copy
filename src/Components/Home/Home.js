6ddbfa8f99659f2ce590354907d5ad79de6dbe21
juhayerayaz <aiazj97@gmail.com>
2022-12-15 22:38:44 +0600

    all done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import Carousel from '../Carousel/Carousel';
 import BestSelling from './BestSelling';
 import UpdateSec from './UpdateSec';
 
 const Home = () => {
+    const [user] = useAuthState(auth);
     return (
         <>
-            <Carousel />
+            {user ? <Carousel /> : <></>}
             <UpdateSec />
             <BestSelling />
         </>
